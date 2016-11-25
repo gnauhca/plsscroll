276ef43ab8ec56f9a4b1c2faac0c20ef1185d30d
zhzhchwin <576073927@qq.com>
2016-11-26 00:53:12 +0800

    add effect

--- effects.js
+++ effects.js
@@ -1,7 +1,120 @@
 var effects = [
     {
+        name: 'pull',
+        effect: function($current, $next, dir, done) {
+            var dur = 1400;
+            if (dir > 0) {
+                $current.addClass('pull-pos-current-final');
+                $next.addClass('pull-pos-next-init');
+            } else {
+                $current.addClass('pull-neg-current-final');
+                $next.addClass('pull-neg-next-init');       
+            }
+
+            setTimeout(function() {
+                if (dir > 0) {
+                    $next.addClass('pull-pos-next-final');
+                } else {
+                    $next.addClass('pull-neg-next-final');       
+                }
+            }, 10);
+            $next
+            .emulateTransitionEnd(dur)
+            .one('bsTransitionEnd', function() {
+                done();
+            });
+        }
+    },
+    {
+        name: 'rotate',
+        effect: function($current, $next, dir, done) {
+            var dur = 1000;
+            if (dir > 0) {
+                $current.addClass('rotate-pos-current-final');
+                $next.addClass('rotate-pos-next-init');
+            } else {
+                $current.addClass('rotate-neg-current-final');
+                $next.addClass('rotate-neg-next-init');       
+            }
+
+            setTimeout(function() {
+                if (dir > 0) {
+                    $next.addClass('rotate-pos-next-final');
+                } else {
+                    $next.addClass('rotate-neg-next-final');       
+                }
+            }, 10);
+            $next
+            .emulateTransitionEnd(dur)
+            .one('bsTransitionEnd', function() {
+                done();
+            });
+        }
+    },
+    {
+        name: 'cube',
+        effect: function($current, $next, dir, done) {
+            var dur = 1000;
+            var height = $current.height();
+            var radius = height * Math.sqrt(3) /2;
+
+            if (dir > 0) {
+                $current.css({
+                    'transform-origin': '50% 50% ' + -radius + 'px',
+                    'transition': 'all 1s'
+                });
+                $next.css({
+                    'transform-origin': '50% 50% ' + -radius + 'px',
+                    'transform': 'rotateX(-60deg)',
+                    'opacity': 0,
+                    'transition': 'all 1s'
+                });
+                setTimeout(function() {
+                    $current.css({
+                        'transform': 'rotateX(60deg)',
+                        'opacity': 0
+                    });
+                    $next.css({
+                        'transform-origin': '50% 50% ' + -radius + 'px',
+                        'opacity': 1,
+                        'transform': 'rotateX(0deg)',
+                    });
+                }, 100);
+            } else {
+                $current.css({
+                    'transform-origin': '50% 50% ' + -radius + 'px',
+                    'transition': 'all 1s'
+                });
+                $next.css({
+                    'transform-origin': '50% 50% ' + -radius + 'px',
+                    'transform': 'rotateX(60deg)',
+                    'opacity': 0,
+                    'transition': 'all 1s'
+                });
+                setTimeout(function() {
+                    $current.css({
+                        'transform': 'rotateX(-60deg)',
+                        'opacity': 0
+                    });
+                    $next.css({
+                        'transform-origin': '50% 50% ' + -radius + 'px',
+                        'opacity': 1,
+                        'transform': 'rotateX(0deg)',
+                    });
+                }, 100);    
+            }
+
+            $next
+            .emulateTransitionEnd(dur + 1000)
+            .one('bsTransitionEnd', function() {
+                done();
+            });
+        }
+    },
+
+    {
         name: 'down-up',
-        effect: function($current, $next, dir, callback) {
+        effect: function($current, $next, dir, done) {
             var dur = 1000;
             if (dir > 0) {
                 $current.addClass('down-up-pos-current-final');
@@ -21,11 +134,9 @@ var effects = [
                 $current
                 .emulateTransitionEnd(dur)
                 .one('bsTransitionEnd', function() {
-                    callback();
+                    done();
                 });
             }, 10);
-
-
         }
     }
 ]
